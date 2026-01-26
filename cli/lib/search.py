d0135b5c3d0d19ffaa3744c72f05997a64a62ce7
mattls <mattias.swenne@gmail.com>
2026-01-26 13:15:53 +0100

    Keyword Search

---
+++ cli/lib/search.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+def search(title) -> None:
+    file_path_abs = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/movies.json"))
+    with open(file_path_abs, "r") as file:
+        movies = json.load(file)
+
+    results = []
+
+    for movie in movies["movies"]:
+        if title in movie["title"]:
+            results.append(movie)
+
+    results = sorted(results,key=lambda movie: movie["id"])
+    results = results[:5]
+    for movie in results:
+        print(f"{movie["id"]}. {movie["title"]} {movie["id"]}")
