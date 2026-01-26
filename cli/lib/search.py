0a1c1ff807a2a29b9285b5658a17ab0a70c14400
mattls <mattias.swenne@gmail.com>
2026-01-26 13:58:46 +0100

    Punctuation

--- cli/lib/search.py
+++ cli/lib/search.py
@@ -1,5 +1,6 @@
 import json
 import os
+import string
 
 def search(title) -> None:
     file_path_abs = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/movies.json"))
@@ -8,11 +9,21 @@ def search(title) -> None:
 
     results = []
 
+    processed_title = process_string(title)
+
     for movie in movies["movies"]:
-        if title.lower() in movie["title"].lower():
+        if processed_title in process_string(movie["title"]):
             results.append(movie)
 
     results = sorted(results,key=lambda movie: movie["id"])
     results = results[:5]
     for movie in results:
         print(f"{movie["id"]}. {movie["title"]} {movie["id"]}")
+
+def process_string(query) -> str:
+    punctiations = {}
+    for char in string.punctuation:
+        punctiations[char] = None
+
+    processed_query = query.translate(str.maketrans(punctiations)).lower()
+    return processed_query
